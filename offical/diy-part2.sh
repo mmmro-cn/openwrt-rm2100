2a62666981688b82750fc079fbe0e64fea0aba03
mmmro-cn <brower.cn@gmail.com>
2025-07-08 15:00:36 +0800

    Update and rename diy2 to diy-part2.sh

--- offical/diy-part2.sh
+++ offical/diy-part2.sh
@@ -15,3 +15,10 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 下载并部署 Tailscale
+curl -L https://github.com/adyanth/openwrt-tailscale-enabler/releases/download/v1.60.0/openwrt-tailscale-enabler-1.60.0_mipsel_24kbfs.tgz \
+  | tar -zx -C /
+opkg update
+opkg install kmod-tun
+/etc/init.d/tailscale enable
